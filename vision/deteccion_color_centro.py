0db5085a3e8c9cdd8fbaf0e7efcd2f6b193edbfc
Juanpa Barías <jpl.barias@uc.cl>
2023-10-25 11:24:04 -0300

    vision adaptada a color de pelota golf

--- vision/deteccion_color_centro.py
+++ vision/deteccion_color_centro.py
@@ -1,6 +1,37 @@
 import cv2
 import numpy as np
 
+def nothing(x):
+    pass
+
+# Load image
+image = cv2.imread('hsv_color_map.png')
+
+# Create a window
+cv2.namedWindow('image')
+
+# Create trackbars for color change
+# Hue is from 0-179 for Opencv
+cv2.createTrackbar('HMin', 'image', 0, 179, nothing)
+cv2.createTrackbar('SMin', 'image', 0, 255, nothing)
+cv2.createTrackbar('VMin', 'image', 0, 255, nothing)
+cv2.createTrackbar('HMax', 'image', 0, 179, nothing)
+cv2.createTrackbar('SMax', 'image', 0, 255, nothing)
+cv2.createTrackbar('VMax', 'image', 0, 255, nothing)
+
+# Set default value for Max HSV trackbars
+cv2.setTrackbarPos('HMax', 'image', 96)
+cv2.setTrackbarPos('SMax', 'image', 255)
+cv2.setTrackbarPos('VMax', 'image', 255)
+cv2.setTrackbarPos('HMin', 'image', 49)
+cv2.setTrackbarPos('SMin', 'image', 45)
+cv2.setTrackbarPos('VMin', 'image', 0)
+
+# Initialize HSV min/max values h49 s85
+hMin = sMin = vMin = hMax = sMax = vMax = 0
+phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+
 cap = cv2.VideoCapture(0)
 
 azulBajo = np.array([100, 100, 20], np.uint8)
@@ -9,20 +40,40 @@ azulAlto = np.array([125, 255, 255], np.uint8)
 verdeBajo = np.array([40, 120, 20], np.uint8)
 verdeAlto = np.array([120, 210, 255], np.uint8)
 
-while True:    
+while True:
+
+    # Get current positions of all trackbars
+    hMin = cv2.getTrackbarPos('HMin', 'image')
+    sMin = cv2.getTrackbarPos('SMin', 'image')
+    vMin = cv2.getTrackbarPos('VMin', 'image')
+    hMax = cv2.getTrackbarPos('HMax', 'image')
+    sMax = cv2.getTrackbarPos('SMax', 'image')
+    vMax = cv2.getTrackbarPos('VMax', 'image')
+
+    # Set minimum and maximum HSV values to display
+    lower = np.array([hMin, sMin, vMin])
+    upper = np.array([hMax, sMax, vMax])
+
+    # Convert to HSV format and color threshold
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+    result = cv2.bitwise_and(image, image, mask=mask)
+
+    cv2.imshow('image', result)    
 
     ret, frame = cap.read()
     
     if ret == True:
         frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(frameHSV, verdeBajo, verdeAlto)
+        mask = cv2.inRange(frameHSV, lower, upper)
         contornos,_ = cv2.findContours(
             mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
         )
         #cv2.drawContours(frame, contornos, -1, (255, 0, 0), 3)
         for c in contornos:
             area = cv2.contourArea(c)
-            if area > 3000:
+            if area > 1000:
+                
                 M = cv2.moments(c)
                 if (M["m00"] == 0):
                     M["m00"] = 1
