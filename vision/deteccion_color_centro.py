dacf27a74a99de60414232efabdd76bd94ce7d8c
Juanpa Barías <jpl.barias@uc.cl>
2023-10-25 12:02:13 -0300

    código visión con opcion de setear y no setear

--- vision/deteccion_color_centro.py
+++ vision/deteccion_color_centro.py
@@ -1,65 +1,74 @@
 import cv2
 import numpy as np
 
+mostrar_contorno = False
+setear_colores = False
+
 def nothing(x):
     pass
 
-# Load image
-image = cv2.imread('hsv_color_map.png')
+if setear_colores:
+    # Load image
+    image = cv2.imread('hsv_color_map.png')
 
-# Create a window
-cv2.namedWindow('image')
+    # Create a window
+    cv2.namedWindow('image')
 
-# Create trackbars for color change
-# Hue is from 0-179 for Opencv
-cv2.createTrackbar('HMin', 'image', 0, 179, nothing)
-cv2.createTrackbar('SMin', 'image', 0, 255, nothing)
-cv2.createTrackbar('VMin', 'image', 0, 255, nothing)
-cv2.createTrackbar('HMax', 'image', 0, 179, nothing)
-cv2.createTrackbar('SMax', 'image', 0, 255, nothing)
-cv2.createTrackbar('VMax', 'image', 0, 255, nothing)
+    # Create trackbars for color change
+    # Hue is from 0-179 for Opencv
+    cv2.createTrackbar('HMin', 'image', 0, 179, nothing)
+    cv2.createTrackbar('SMin', 'image', 0, 255, nothing)
+    cv2.createTrackbar('VMin', 'image', 0, 255, nothing)
+    cv2.createTrackbar('HMax', 'image', 0, 179, nothing)
+    cv2.createTrackbar('SMax', 'image', 0, 255, nothing)
+    cv2.createTrackbar('VMax', 'image', 0, 255, nothing)
 
-# Set default value for Max HSV trackbars
-cv2.setTrackbarPos('HMax', 'image', 96)
-cv2.setTrackbarPos('SMax', 'image', 255)
-cv2.setTrackbarPos('VMax', 'image', 255)
-cv2.setTrackbarPos('HMin', 'image', 49)
-cv2.setTrackbarPos('SMin', 'image', 45)
-cv2.setTrackbarPos('VMin', 'image', 0)
+    # Set default value for Max HSV trackbars
+    cv2.setTrackbarPos('HMax', 'image', 96)
+    cv2.setTrackbarPos('SMax', 'image', 255)
+    cv2.setTrackbarPos('VMax', 'image', 255)
+    cv2.setTrackbarPos('HMin', 'image', 49)
+    cv2.setTrackbarPos('SMin', 'image', 45)
+    cv2.setTrackbarPos('VMin', 'image', 0)
 
-# Initialize HSV min/max values h49 s85
-hMin = sMin = vMin = hMax = sMax = vMax = 0
-phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+    # Initialize HSV min/max values h49 s85
+    hMin = sMin = vMin = hMax = sMax = vMax = 0
+    phMin = psMin = pvMin = phMax = psMax = pvMax = 0
 
 
 cap = cv2.VideoCapture(0)
 
-azulBajo = np.array([100, 100, 20], np.uint8)
-azulAlto = np.array([125, 255, 255], np.uint8)
+# azulBajo = np.array([100, 100, 20], np.uint8)
+# azulAlto = np.array([125, 255, 255], np.uint8)
 
-verdeBajo = np.array([40, 120, 20], np.uint8)
-verdeAlto = np.array([120, 210, 255], np.uint8)
+# verdeBajo = np.array([40, 120, 20], np.uint8)
+# verdeAlto = np.array([120, 210, 255], np.uint8)
 
 while True:
 
-    # Get current positions of all trackbars
-    hMin = cv2.getTrackbarPos('HMin', 'image')
-    sMin = cv2.getTrackbarPos('SMin', 'image')
-    vMin = cv2.getTrackbarPos('VMin', 'image')
-    hMax = cv2.getTrackbarPos('HMax', 'image')
-    sMax = cv2.getTrackbarPos('SMax', 'image')
-    vMax = cv2.getTrackbarPos('VMax', 'image')
+    if setear_colores:
+        # Get current positions of all trackbars
+        hMin = cv2.getTrackbarPos('HMin', 'image')
+        sMin = cv2.getTrackbarPos('SMin', 'image')
+        vMin = cv2.getTrackbarPos('VMin', 'image')
+        hMax = cv2.getTrackbarPos('HMax', 'image')
+        sMax = cv2.getTrackbarPos('SMax', 'image')
+        vMax = cv2.getTrackbarPos('VMax', 'image')
 
-    # Set minimum and maximum HSV values to display
-    lower = np.array([hMin, sMin, vMin])
-    upper = np.array([hMax, sMax, vMax])
+        # Set minimum and maximum HSV values to display
+        lower = np.array([hMin, sMin, vMin])
+        upper = np.array([hMax, sMax, vMax])
 
-    # Convert to HSV format and color threshold
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower, upper)
-    result = cv2.bitwise_and(image, image, mask=mask)
+        # Convert to HSV format and color threshold
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower, upper)
+        result = cv2.bitwise_and(image, image, mask=mask)
 
-    cv2.imshow('image', result)    
+        
+        cv2.imshow('image', result)
+    else:    
+        lower = np.array([49, 45, 0], np.uint8)
+        upper = np.array([96, 255, 255], np.uint8)
 
     ret, frame = cap.read()
     
@@ -84,7 +93,7 @@ while True:
                 cv2.putText(frame, '{},{}'.format(x, y), (x+10, y), font, 0.75, (255,0,255), 1, cv2.LINE_AA)
                 nuevoContorno = cv2.convexHull(c)
                 cv2.circle(frame, (x,y), max(nuevoContorno[:, 0, 0].tolist()) - x, (0,0,255), 2)
-                mostrar_contorno = False
+                
                 if mostrar_contorno:
                     cv2.drawContours(frame, [nuevoContorno], 0, (0, 255, 0), 3)
                 print(f"Distancia con respecto al centro de la imagen: {x - frame.shape[1]*0.5}")
