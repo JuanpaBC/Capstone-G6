4cc501265d8392b6e9d7d42ac66d1f7d9bfc6496
pincheirapatagonia <mdpincheira@uc.cl>
2023-12-03 02:43:34 -0300

    tengo tuto, pero PID de camara

--- Tests_PID/PID_cam.py
+++ Tests_PID/PID_cam.py
@@ -1,3 +1,27 @@
+
+def theta_error(x, y, x_target, y_target):
+    # Returns the error in the angle theta
+    angle = math.atan2(y_target - y, x_target - x)*180/math.pi
+    if-7.5 < angle < 7.5:
+        return 0
+    else:
+        return angle/abs(angle)
+    
+
+def lineal_error(x, y, x_target, y_target):
+    # Returns the error in the lineal distance
+    return math.sqrt((x_target - x)**2 + (y_target - y)**2)
+
+def err_reference(x, y, x_target, y_target):
+    # Returns the reference for the PID
+    e_t = theta_error(x, y, x_target, y_target)
+    e_l = lineal_error(x, y, x_target, y_target)
+    if e_t == 0:
+        return e_l, e_l
+    else:
+        return e_t*e_l, -e_t*e_l
+
+
 class PID:
     def __init__(self, kp=0.0, ki=0.0, kd=0.0):
         self.kp = kp
@@ -6,18 +30,17 @@ class PID:
         self.previous_error = 0.0
         self.integral = 0.0
 
-    def update(self, error, delta_time):
-        self.integral += error * delta_time
-        derivative = (error - self.previous_error) / delta_time
-        output = self.kp * error + self.ki * self.integral + self.kd * derivative
-        self.previous_error = error
-        return output
-
-
-def theta_error(x, y, x_target, y_target):
-    # Returns the error in the angle theta
-    return math.atan2(y_target - y, x_target - x)
-
-def lineal_error(x, y, x_target, y_target):
-    # Returns the error in the lineal distance
-    return math.sqrt((x_target - x)**2 + (y_target - y)**2)
+    def update(self, delta_time, x, y, x_target, y_target, err_reference=err_reference):
+        errA, errB = err_reference(x, y, x_target, y_target)
+        self.integralA += errA * delta_time
+        derivativeA = (errA - self.previous_errA) / delta_time
+        outputA = self.kp * errA + self.ki * self.integralA + self.kd * derivativeA
+        self.previous_errA = errA
+        
+    
+        self.integralA += errB * delta_time
+        derivativeB = (errB - self.previous_errB) / delta_time
+        outputB = self.kp * errB + self.ki * self.integralB + self.kd * derivativeB
+        self.previous_errB = errB
+        
+        return outputA, outputB
