f7de59bf25be4073c69b9ac23e5a61e86410f578
pincheirapatagonia <mdpincheira@uc.cl>
2023-12-09 20:18:22 -0300

    video record Same As Main

--- Tests/Vision/video_record.py
+++ Tests/Vision/video_record.py
@@ -2,8 +2,10 @@ from picamera2.encoders import H264Encoder
 from picamera2 import Picamera2, Preview
 import time
 picam2 = Picamera2()
-video_config = picam2.create_video_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
-picam2.configure(video_config)
+picam2.resolution = (320, 240)
+picam2.framerate = 30
+preview_config = picam2.create_preview_configuration()
+picam2.configure(preview_config)
 encoder = H264Encoder(bitrate=10000000)
 output = "test.h264"
 picam2.start_preview(Preview.QTGL)
