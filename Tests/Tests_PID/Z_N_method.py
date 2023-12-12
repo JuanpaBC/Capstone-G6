63494747f59cf8285f8e84c7b184d26662fc757a
pincheirapatagonia <mdpincheira@uc.cl>
2023-12-12 01:49:44 -0300

    nestooor

--- Tests/Tests_PID/Z_N_method.py
+++ Tests/Tests_PID/Z_N_method.py
@@ -52,9 +52,10 @@ csv_file_path = 'serial_data.csv'
 csv_header = ['Time', 'KpB', 'RPMB', 'BRPMref']
 
 # External variables
-KpB_valuesref = np.linspace(0.0, 5.0, 230).tolist()  # List of 100 values from 0.0 to 0.1
+Kp_valuesref = np.linspace(0.0, 5.0, 230).tolist()  # List of 100 values from 0.0 to 0.1
 KpB_values = []
 RPMB_values = []
+RPMA_values = []
 RPMref_values = []
 v_ref = str(250)
 i  = 0
@@ -65,30 +66,32 @@ try:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(csv_header)
 
-        for KpB in KpB_valuesref:
+        for KpB in Kp_valuesref:
+            
             start_time = time.time()
-            while True:
-                current_time = time.time()
-                elapsed_time = current_time - start_time
-                if elapsed_time >= duration:
-                    break
-                if (len(coms.data.split(',')) >= 2 and coms.data != last_data):
-                    # Extract KpB, RPMB, RPMref from the updated 'data'
-                    Time, Aparts, Bparts = coms.data.split(',')
-                    refA, RPMA, kparec = Aparts.split('|')
-                    refB, RPMB, kpbrec = Bparts.split('|')
-                        # Assuming KpB is the third part of bData
-                    # Save data to lists
-                    RPMB_values.append(RPMB.split(':')[1].strip())
-                    RPMref_values.append(refB.split(':')[1].strip())
-                    KpB_values.append(kpbrec.split(':')[1].strip())  # Update KpB_values in the same loop
-                    # Save data to CSV
-                    csv_writer.writerow([Time, kpbrec.split(':')[1].strip(), RPMB.split(':')[1].strip(), refB.split(':')[1].strip()])
-                    last_data = coms.data
-                    i = i+1
-                coms.comunicacion(
-                    f"{str(round(KpB, 3))},{v_ref},{str(round(KpB, 3))},{v_ref}")
-                coms.read_and_print_messages()
+            
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+            if elapsed_time >= duration:
+                break
+            if (len(coms.data.split(',')) >= 2 and coms.data != last_data):
+                # Extract KpB, RPMB, RPMref from the updated 'data'
+                Time, Aparts, Bparts = coms.data.split(',')
+                refA, RPMA, kparec = Aparts.split('|')
+                refB, RPMB, kpbrec = Bparts.split('|')
+                    # Assuming KpB is the third part of bData
+                # Save data to lists
+                RPMB_values.append(RPMB.split(':')[1].strip())
+                RPMA_values.append(RPMA.split(':')[1].strip())
+                RPMref_values.append(refB.split(':')[1].strip())
+                KpB_values.append(kpbrec.split(':')[1].strip())  # Update KpB_values in the same loop
+                # Save data to CSV
+                csv_writer.writerow([Time, kpbrec.split(':')[1].strip(), RPMB.split(':')[1].strip(), refB.split(':')[1].strip()])
+                last_data = coms.data
+                i = i+1
+            coms.comunicacion(
+                f"{str(round(KpB, 3))},{v_ref},{str(round(KpB, 3))},{v_ref}")
+            coms.read_and_print_messages()
 
 except KeyboardInterrupt:
     print("Data collection interrupted.")
@@ -96,8 +99,9 @@ except KeyboardInterrupt:
 finally:
     coms.arduino.close()
 
+plt.plot(KpB_values, RPMA_values, label='RPMA vs KpB')
 plt.plot(KpB_values, RPMB_values, label='RPMB vs KpB')
 plt.xlabel('KpB')
-plt.ylabel('RPMB')
+plt.ylabel('RPM')
 plt.legend()
 plt.savefig("zn.jpg")
